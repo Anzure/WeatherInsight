d46ae13c6cb55220c0c6a7ce97140565d3376fca
André Mathisen <andre.mathisen@odit.no>
2023-11-20 14:08:20 +0100

    Loop through forecast list

--- modelcontroller.cpp
+++ modelcontroller.cpp
@@ -4,6 +4,8 @@
 #include <QJsonDocument>
 #include <QJsonObject>
 #include <QDebug>
+#include <QJsonArray>
+#include <QJsonValue>
 
 ModelController::ModelController(QObject *parent) : QObject(parent)
 {
@@ -16,8 +18,8 @@ void ModelController::getWeatherData(QString city)
 {
     QString apiUrl = "https://api.openweathermap.org/data/2.5";
     QString apiKey = "22bd0da1b9e4b48079ec27a4b17235d6";
-    QString url(apiUrl + "/weather?q=" + city + "&appid=" + apiKey);
-    qInfo() << "GET " << url;
+    QString url(apiUrl + "/forecast?q=" + city + "&appid=" + apiKey);
+    qInfo() << "GET " + url;
     QNetworkRequest request(url);
     networkManager->get(request);
 }
@@ -34,5 +36,21 @@ void ModelController::onNetworkReply(QNetworkReply *reply)
 
     QJsonDocument jsonResponse = QJsonDocument::fromJson(responseStr.toUtf8());
     QJsonObject jsonObject = jsonResponse.object();
+
+    QJsonArray dataPointList = jsonObject["list"].toArray();
+    for (const QJsonValue &value : dataPointList) {
+        QJsonObject dataPointObj = value.toObject();
+        QString timestamp = dataPointObj["dt_txt"].toString();
+        QJsonArray weatherArray = dataPointObj["weather"].toArray();
+
+        QString weatherDesc;
+        for (const QJsonValue &value : weatherArray) {
+            QJsonObject weatherObj = value.toObject();
+            weatherDesc.append(weatherObj["description"].toString() + ",");
+        }
+        weatherDesc.removeLast();
+
+        qInfo() << timestamp + " " + weatherDesc;
+    }
 }
 
